7e271aa5079da93ad0aea0d2ae1e56c5b0890393
CayoWalterJulian <catojulian15@gmail.com>
2023-08-13 00:50:15 -0300

    a

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get ("/" ,(req, res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get ("/register" ,(req, res)=>{
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+app.get ("/login" ,(req, res)=>{
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.listen(port , () => console.log('Servidor corriendo en el puerto ${port}'))
