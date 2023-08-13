e52ef81929ed7be16809239bc6adc40aa49b5e79
CayoWalterJulian <catojulian15@gmail.com>
2023-08-12 23:53:40 -0300

    se agrega archivos html css de pagina mercado libre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get ("/" ,(req, res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get ("/register" ,(req, res)=>{
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+app.get ("/login" ,(req, res)=>{
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.listen(4321 , () =>{
+    console.log('vamos alla');
+})
